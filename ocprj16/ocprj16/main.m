08f823f7bbcab016d15bafbde89d06084277a3b9
zzxb <cnhidba@126.com>
2016-09-03 22:20:07 +0800

    no message

---
+++ ocprj16/ocprj16/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocprj16
+//
+//  Created by zzxb on 16/9/3.
+//  Copyright © 2016年 zzxb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
