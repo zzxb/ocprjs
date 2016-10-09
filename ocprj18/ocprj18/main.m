aef4013c1b538bb5a690e02170369541e3d8c9f0
zzxb <cnhidba@126.com>
2016-10-10 07:38:51 +0800

    新增利用NSRunLoop调用AFNetworking框架，并解析JSON

--- ocprj18/ocprj18/main.m
+++ ocprj18/ocprj18/main.m
@@ -7,11 +7,23 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "AFHTTPSessionManager.h"
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    do {
+        @autoreleasepool
+        {
+            AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+            [manager GET:@"http://www.weather.com.cn/data/sk/101010100.html" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                NSData *jsonData = [NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil];
+                
+                NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+                NSLog(@"这是全部数据%@",jsonStr);
+            } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                NSLog(@"Error: %@", error);
+            }];
+            [[NSRunLoop currentRunLoop] run];
+        }
+    } while (YES);
+
     return 0;
 }
