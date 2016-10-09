56d65ada78783f06cf80ee136d3b9c56bd25bfd1
zzxb <cnhidba@126.com>
2016-10-09 22:56:20 +0800

    基于AFNetworking框架的web service调用

---
+++ ocprj18/ocprj18/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ocprj18
+//
+//  Created by zzxb on 16/10/9.
+//  Copyright © 2016年 zzxb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
